230ed8bbbc6840f5931383a01d8b52de6db65acf
jwonnyleaf <jwonnyleaf@gmail.com>
2021-09-11 16:55:11 -0500

    removed new.html

--- app.py
+++ app.py
@@ -6,10 +6,5 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/test")
-def test():
-    return render_template("new.html")
-
-
 if __name__ == "__main__":
     app.run(debug = True)
