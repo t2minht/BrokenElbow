8eec8573b2d047c57925975b25c8f2a395a3145b
jwonnyleaf <jwonnyleaf@gmail.com>
2021-09-11 17:31:04 -0500

    something

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ def home():
     return render_template("index.html")
 
 @app.route("/search", methods = ["POST", "GET"])
-def login():
+def search():
     if request.method == "POST":
-        userInput = request.form["nm"]
-        print(userInput)
+        userArtist = request.form.get['userArtist']
+        userSong = request.form.get['userSong']
+        print(userArtist, userSong)
     return render_template("search.html")
-
 if __name__ == "__main__":
     app.run(debug = True)
