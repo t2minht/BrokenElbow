3be693652fb1c06bd3950a74827422a8ad955433
Brian Vu <brianminhvu@gmail.com>
2021-09-11 22:42:40 -0500

    bruh

--- app.py
+++ app.py
@@ -3,14 +3,10 @@ from flask import Flask, redirect, url_for, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return render_template("index.html")
-
-@app.route("/search")
 def search():
     return render_template("search.html")
 
-@app.route("/search", methods = ["POST"])
+@app.route("/", methods = ["POST"])
 def search_post():
         artistName = request.form.get('artistName')
         songName = request.form.get('songName')
